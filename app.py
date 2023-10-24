53a9069dfd32f1a010c7aa7af64b9bbfac7cfa70
mustafa taner <19351525@ogr.dicle.edu.tr>
2023-10-24 22:56:16 +0300

    app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import os
-from apikey import apikey
 import streamlit as st
 from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
@@ -9,20 +8,24 @@ from langchain.memory import ConversationBufferMemory
 from langchain.utilities import WikipediaAPIWrapper
 
 
-#os.environ['OPENAI_API_KEY']=apikey
 apikey = os.environ.get("OPENAI_API_KEY")
+
+
 st.title("Kaçak Zeka'ya Hoş Geldiniz!")
 st.caption('bu proje :blue[mustafa taner]  tarafından yazılmıştır   :sunglasses:')
 st.balloons()
-prompt=st.text_input('Bana istediğin soruyu sorabilirsin :) ')
+prompt = st.text_input('Bana istediğin soruyu sorabilirsin :) ')
 
+# LLM nesnesini başlat
+llm = ChatOpenAI(temperature=0.92, model_name='gpt-3.5-turbo')
 
 if st.button('Sor'):
     if prompt:
-        llm = ChatOpenAI(temperature=0.92, model_name='gpt-3.5-turbo')
-        response = llm(prompt)
-        st.write(response)
+        # Mesaj formatını doğru bir şekilde ayarla
+        response = llm([{"role": "user", "content": prompt}])
+        st.write(response['choices'][0]['message']['content'])
     else:
         st.write("Lütfen bir soru girin.")
+
 
 
