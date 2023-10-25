00223c38a5b712b538505ece6ee784b9e8342dbd
mustafa taner <19351525@ogr.dicle.edu.tr>
2023-10-25 10:12:01 +0300

    app.py

--- app.py
+++ app.py
@@ -1,33 +1,24 @@
 import os
+from apikey import apikey
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
 
-# API anahtarını al
+
+
+#os.environ['OPENAI_API_KEY']=apikey
 apikey = os.environ.get("OPENAI_API_KEY")
-
 st.title("Kaçak Zeka'ya Hoş Geldiniz!")
 st.caption('bu proje :blue[mustafa taner]  tarafından yazılmıştır   :sunglasses:')
 st.balloons()
-prompt = st.text_input('Bana istediğin soruyu sorabilirsin :) ')
-
-# LLM nesnesini başlat
-llm = ChatOpenAI(temperature=0.92, model_name='gpt-3.5-turbo')
-
-# Sohbet geçmişini (conversation history) tutan bir liste oluştur
-conversation_history = []
-
+prompt=st.text_input('Bana istediğin soruyu sorabilirsin :) ')
 if st.button('Sor'):
     if prompt:
-        # Yeni mesajı sohbet geçmişine ekle
-        conversation_history.append({"role": "user", "content": prompt})
-        
-        # Modeli bu sohbet geçmişiyle çağır
-        response = llm(conversation_history)
-        
-        # Modelin verdiği yanıtı sohbet geçmişine ve ekrana ekle
-        answer = response['choices'][0]['message']['content']
-        conversation_history.append({"role": "assistant", "content": answer})
-        
-        st.write(answer)
+        llm = OpenAI(temperature=0.92, model_name='gpt-3.5-turbo')
+        response = llm(prompt)
+        st.write(response)
     else:
         st.write("Lütfen bir soru girin.")
